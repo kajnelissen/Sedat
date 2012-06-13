496925dc647850f89aa64357e3726e1149776afe
Kaj Nelissen <kaj@kj91.nl>
2012-06-13 11:37:47 +0200

    JAWOHL

---
+++ PipeFilterImplementation/Pipes/IPipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pipes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class IPipe
+    {
+    }
+}
---
+++ PipeFilterImplementation/Simulation/SimpleSim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SimpleSim : ISim
+    {
+    }
+}
