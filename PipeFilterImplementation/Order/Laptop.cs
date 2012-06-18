1b1ec1d7f24aaab01a67e639a8da62111b46f9a0
Kaj Nelissen <kaj@kj91.nl>
2012-06-18 10:45:25 +0200

    IPipe interface gedefinieerd
    Methodes voor pipes zijn gedefinieerd.

--- PipeFilterImplementation/Order/Laptop.cs
+++ PipeFilterImplementation/Order/Laptop.cs
@@ -5,14 +5,28 @@ using System.Text;
 
 namespace Order
 {
+    /// <summary>
+    /// Representeert een laptopsysteem.
+    /// </summary>
     public class Laptop : AbstractOrder
     {
+        /// <summary>
+        /// Construeert nieuw laptopsysteem met
+        /// meegegeven order ID.
+        /// </summary>
+        /// <param name="orderId">Order ID</param>
         public Laptop(int orderId)
             : base(orderId)
         {
             this._type = "Laptop";
         }
 
+        /// <summary>
+        /// Construeert nieuw laptopsysteem met
+        /// meegegeven order ID en status.
+        /// </summary>
+        /// <param name="orderId">Order ID</param>
+        /// <param name="os">Beginstatus van order</param>
         public Laptop(int orderId, OrderStatus os)
             : base(orderId, os)
         {
--- PipeFilterImplementation/Pipes/IPipe.cs
+++ PipeFilterImplementation/Pipes/IPipe.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Filters;
+using Order;
 
 namespace Pipes
 {
@@ -10,5 +12,57 @@ namespace Pipes
     /// </summary>
     public abstract class IPipe
     {
+        /// <summary>
+        /// Filter waar deze pipe data
+        /// vandaan haalt.
+        /// </summary>
+        private IFilter from;
+
+        /// <summary>
+        /// Filter waar deze pipe data
+        /// naartoe transporteert.
+        /// </summary>
+        private IFilter to;
+
+        /// <summary>
+        /// Construeert nieuwe pipe zonder
+        /// verbindingen met filters.
+        /// </summary>
+        public IPipe()
+        {
+            this.from = null;
+            this.to = null;
+        }
+
+        /// <summary>
+        /// Construeert een nieuwe pipe en laat deze
+        /// de meegegeven filters verbinden.
+        /// </summary>
+        /// <param name="from">Filter waar data vandaan komt</param>
+        /// <param name="to">Filter waar data naartoe gaat</param>
+        public IPipe(IFilter from, IFilter to)
+        { 
+            
+        }
+
+        public void Connect(IFilter from, IFilter to)
+        { 
+            
+        }
+
+        public void SetStartPoint(IFilter from)
+        { 
+            
+        }
+
+        public void SetEndPoint(IFilter to)
+        { 
+            
+        }
+
+        public void Transport()
+        { 
+            
+        }
     }
 }
